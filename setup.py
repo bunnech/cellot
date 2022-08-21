0abfb5a9ad92e6c661597c71c9806da43efc54b3
Charlotte Bunne <bunnech@users.noreply.github.com>
2022-08-21 16:34:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # !/usr/bin/python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='cellot',
       version='0.1',
@@ -9,8 +9,8 @@ setup(name='cellot',
       author='Charlotte Bunne, Stefan G. Stark',
       author_email='bunnec@ethz.ch, starks@ethz.ch',
       license='BSD',
-      install_requires=[],
-      py_modules=['cellot'],
+      packages=find_packages(include=['cellot', 'cellot.*']),
+      install_requires=[l.strip() for l in Path("requirements.txt").read_text("utf-8").splitlines()],
       long_description=open('READme.md').read(),
       zip_safe=False,
 )
